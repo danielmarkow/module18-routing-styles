b22493b1975ebe8e156be394281468bc2fec6962
Daniel Markow <daniel.markow@outlook.com>
2022-09-28 21:47:31 +0200

    style basics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./components/Home";
 import About from "./components/About";
 import Products from "./components/Products";
+import Nav from "./components/Nav";
 
 import {Routes, Route, Link, HashRouter} from "react-router-dom";
 
@@ -9,23 +10,21 @@ import {UserContext} from "./context/UserContext";
 function App() {
 
     return (
-    <HashRouter>
-        <div>
-            <h1>Routing - Hello World</h1>
-            <Link to="/">Home</Link> ---
-            <Link to="/about">About</Link> ---
-            <Link to="/products">Products</Link>
-            <hr />
-            <UserContext.Provider value={{users: ["peter"]}}>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/products" element={<Products />} />
-                </Routes>
-            </UserContext.Provider>
+        <HashRouter>
+            <div>
+                <h1>Routing - Hello World</h1>
+                <Nav />
+                <hr />
+                <UserContext.Provider value={{users: ["peter"]}}>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/products" element={<Products />} />
+                    </Routes>
+                </UserContext.Provider>
 
-        </div>
-    </HashRouter>
+            </div>
+        </HashRouter>
     );
 }
 
